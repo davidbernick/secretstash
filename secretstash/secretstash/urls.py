b8df06c4f2088a63a1fadfd277194965824fb570
davidbernick <dbernick@gmail.com>
2014-08-04 11:25:50 -0400

    initial commit

---
+++ secretstash/secretstash/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'secretstash.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
