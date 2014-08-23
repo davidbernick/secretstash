805a4f20854ea720278068d3ccdb5f2610b7a181
David Bernick <dbernick@gmail.com>
2014-08-22 22:41:03 -0400

    added encrypt dir

--- secretstash/secretstash/settings/local.py
+++ secretstash/secretstash/settings/local.py
@@ -62,3 +62,5 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+ENCRYPTED_FIELDS_KEYDIR = '/opt/kz'
+
