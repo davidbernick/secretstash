fd1f97f28a4e070ffe99dd1094b8cd55d6971d93
David Bernick <dbernick@gmail.com>
2015-02-11 20:21:09 -0500

    Update admin.py

--- secretstash/secrets/admin.py
+++ secretstash/secrets/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Secret,Host
 from guardian.admin import GuardedModelAdmin
+from django.contrib.auth.models import User, Group, Permission
+
 
 class SecretAdmin(GuardedModelAdmin):
     pass
@@ -8,5 +10,9 @@ class SecretAdmin(GuardedModelAdmin):
 class HostAdmin(GuardedModelAdmin):
     pass
 
+class GroupAdmin(GuardedModelAdmin):
+    pass
+
 admin.site.register(Secret,SecretAdmin)
 admin.site.register(Host,HostAdmin)
+admin.site.register(Group,GroupAdmin)
